59ba1159e4c0184ce4c27d668f06125d9e2ae378
s41173 <sanjaya.kiran@gmail.com>
2017-12-31 14:16:53 +0700

    Order Offer

--- application/modules/orders/controllers/orders.php
+++ application/modules/orders/controllers/orders.php
@@ -1,12 +1,12 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Article extends MX_Controller
+class Orders extends MX_Controller
 {
     function __construct()
     {
         parent::__construct();
         
-        $this->load->model('Article_model', '', TRUE);
+        $this->load->model('Orders_model', '', TRUE);
 
         $this->properti = $this->property->get();
 //        $this->acl->otentikasi();
@@ -16,6 +16,7 @@ class Article extends MX_Controller
         $this->role = new Role_lib();
         $this->category = new News_category_lib();
         $this->language = new Language_lib();
+        $this->agent = new Agent_lib();
         
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
@@ -23,20 +24,24 @@ class Article extends MX_Controller
 
     }
 
-    private $properti, $modul, $title;
+    private $properti, $modul, $title, $agent;
     private $role, $category, $language;
 
     // json format
-    function get_article(){
+    function get_orders(){
         
         $datas = (array)json_decode(file_get_contents('php://input'));
-        $uid = $datas['id'];
+        $agent = $datas['agent_id'];
         
-        $res = $this->Article_model->get_by_id($uid)->row();
+        $result = $this->Orders_model->search($agent,'null')->result();
         
-	$output[] = array ("id" => $res->id, "lang" => $res->lang, "permalink" => $res->permalink, "title" => $res->title, 
-                           "text" => $res->text, "dates" => $res->dates, "time" => $res->time,
-                           "image" => base_url().'images/article/'.$res->image);
+        foreach ($result as $res) {
+            
+           if ($res->approved == 1){ $stts = 'Processed'; }else{ $stts = 'On Progress'; } 
+           $output[] = array ("id" => $res->id, "code" => $res->code, "dates" => tglin($res->dates), 
+                              "agent_id" => $this->agent->get_name($res->agent_id), "content" => $res->content,
+                              "status" => $stts, "image" => base_url().'images/offer/'.$res->image);
+        }
         
         $response['content'] = $output;
             $this->output
@@ -47,24 +52,86 @@ class Article extends MX_Controller
             exit;    
     }
     
+    function add_orders(){
+        
+        $datax = (array)json_decode(file_get_contents('php://input')); 
+        
+        $orderid = $this->Orders_model->counter().mt_rand(100,9999);
+        $orders = array('dates' => date('Y-m-d'), 'agent_id' => $datax['agent_id'], 'code' => $orderid,
+                        'content' => $datax['content'], 'created' => date('Y-m-d H:i:s'));
+
+        $this->Orders_model->add($orders);
+        $response = array('status' => true, 'orderid' => $orderid); 
+        
+        $this->output
+        ->set_status_header(201)
+        ->set_content_type('application/json', 'utf-8')
+        ->set_output(json_encode($response))
+        ->_display();
+        exit;
+        
+    }
+    
+    function remove_orders(){
+        
+        $datax = (array)json_decode(file_get_contents('php://input')); 
+        
+        $orderid = $this->Orders_model->get_by_id($datax['id'])->row();
+        if ($orderid->approved == 0){
+            
+            $source = 'images/offer/'.$orderid->image;
+            unlink($source);
+            
+            $this->Orders_model->force_delete($datax['id']);
+            $response = array('status' => true, 'error' => 'Offer Removed..!!'); 
+            
+        }else{ $response = array('status' => false, 'error' => "Offer Already Posted..!!");  }
+
+        $this->output
+        ->set_status_header(201)
+        ->set_content_type('application/json', 'utf-8')
+        ->set_output(json_encode($response))
+        ->_display();
+        exit;
+        
+    }
+    
+    function edit_image_orders(){
+        
+        $datax = (array)json_decode(file_get_contents('php://input')); 
+        
+        $orderid = $this->Orders_model->get_detail_based_order($datax['orderid']);
+        $orders = array('image' => $datax['image']);
+
+        $this->Orders_model->update($orderid->id,$orders);
+        $response = array('status' => true); 
+        
+        $this->output
+        ->set_status_header(201)
+        ->set_content_type('application/json', 'utf-8')
+        ->set_output(json_encode($response))
+        ->_display();
+        exit;
+        
+    }
+    
     function index()
     {
        $this->get_last(); 
     }
      
-    public function getdatatable($search=null,$cat='null',$lang='null',$publish='null',$dates='null')
+    public function getdatatable($search=null,$agent='null',$publish='null')
     {
-        if(!$search){ $result = $this->Article_model->get_last($this->modul['limit'])->result(); }
-        else {$result = $this->Article_model->search($cat,$lang,$publish,$dates)->result(); }
+        if(!$search){ $result = $this->Orders_model->get_last($this->modul['limit'])->result(); }
+        else {$result = $this->Orders_model->search($agent,$publish)->result(); }
 	
         $output = null;
         if ($result){
                 
          foreach($result as $res)
 	 {             
-	   $output[] = array ($res->id, $this->category->get_name($res->category_id), $res->user, $res->lang, $res->permalink, $res->title,
-                              $res->text, $res->image, tglin($res->dates), $res->time, $res->counter, $res->comment, status($res->front), $res->publish,
-                              $res->created, $res->updated, $res->deleted
+	   $output[] = array ($res->id, $res->code, tglin($res->dates), $this->agent->get_name($res->agent_id), $res->content, base_url().'images/orders/'.$res->image,
+                              $res->approved, $res->created, $res->updated, $res->deleted
                              );
 	 } 
          
@@ -82,18 +149,16 @@ class Article extends MX_Controller
         $this->acl->otentikasi1($this->title);
 
         $data['title'] = $this->properti['name'].' | Administrator  '.ucwords('Article Manager');
-        $data['h2title'] = 'Article Manager';
+        $data['h2title'] = 'Orders Manager';
 
-        $data['main_view'] = 'article_view';
+        $data['main_view'] = 'orders_view';
 
 	$data['form_action'] = site_url($this->title.'/add_process');
         $data['form_action_update'] = site_url($this->title.'/update_process');
         $data['form_action_del'] = site_url($this->title.'/delete_all');
         $data['link'] = array('link_back' => anchor('main/','Back', array('class' => 'btn btn-danger')));
 
-        $data['category'] = $this->category->combo_all();
-        $data['language'] = $this->language->combo_all();
-        
+        $data['agent'] = $this->agent->combo_name();
 	// ---------------------------------------- //
  
         $config['first_tag_open'] = $config['last_tag_open']= $config['next_tag_open']= $config['prev_tag_open'] = $config['num_tag_open'] = '<li>';
@@ -109,7 +174,7 @@ class Article extends MX_Controller
         $this->table->set_empty("&nbsp;");
 
         //Set heading untuk table
-        $this->table->set_heading('#','No', 'Category', '#', 'Title', 'Date', 'Time', 'User', 'Front', 'Action');
+        $this->table->set_heading('#','No', 'Code', 'Dates', 'Agent', 'Action');
 
         $data['table'] = $this->table->generate();
         $data['source'] = site_url($this->title.'/getdatatable');
@@ -121,9 +186,9 @@ class Article extends MX_Controller
     function publish($uid = null)
     {
        if ($this->acl->otentikasi2($this->title,'ajax') == TRUE){ 
-       $val = $this->Article_model->get_by_id($uid)->row();
-       if ($val->publish == 0){ $lng = array('publish' => 1); }else { $lng = array('publish' => 0); }
-       $this->Article_model->update($uid,$lng);
+       $val = $this->Orders_model->get_by_id($uid)->row();
+       if ($val->approved == 0){ $lng = array('approved' => 1); }else { $lng = array('approved' => 0); }
+       $this->Orders_model->update($uid,$lng);
        echo 'true|Status Changed...!';
        }else{ echo "error|Sorry, you do not have the right to change publish status..!"; }
     }
@@ -141,7 +206,7 @@ class Article extends MX_Controller
           $x = 0;
           for ($i=0; $i<$jumlah; $i++)
           {
-             $this->Article_model->delete($cek[$i]);
+             $this->Orders_model->delete($cek[$i]);
              $x=$x+1;
           }
           $res = intval($jumlah-$x);
@@ -161,7 +226,7 @@ class Article extends MX_Controller
     function delete($uid)
     {
         if ($this->acl->otentikasi_admin($this->title,'ajax') == TRUE){
-            $this->Article_model->delete($uid);
+            $this->Orders_model->delete($uid);
             
             $this->session->set_flashdata('message', "1 $this->title successfully removed..!");
 
@@ -175,7 +240,7 @@ class Article extends MX_Controller
 
         $data['title'] = $this->properti['name'].' | Administrator  '.ucwords($this->modul['title']);
         $data['h2title'] = 'Create New '.$this->modul['title'];
-        $data['main_view'] = 'article_form';
+        $data['main_view'] = 'orders_form';
 	$data['form_action'] = site_url($this->title.'/add_process');
         $data['link'] = array('link_back' => anchor($this->title,'Back', array('class' => 'btn btn-danger')));
 
@@ -191,27 +256,12 @@ class Article extends MX_Controller
 
     function add_process()
     {
-        if ($this->acl->otentikasi2($this->title) == TRUE){
-
-            $data['title'] = $this->properti['name'].' | Administrator  '.ucwords('Article Manager');
-            $data['h2title'] = 'Article Manager';
-            $data['main_view'] = 'article_form';
-            $data['form_action'] = site_url($this->title.'/add_process');
-            $data['link'] = array('link_back' => anchor('admin/','<span>back</span>', array('class' => 'back')));
-
-            // Form validation
-            
-            $this->form_validation->set_rules('ttitle', 'Article Title', 'required|maxlength[100]|callback_valid');
-            $this->form_validation->set_rules('ccategory', 'Category', 'required');
-            $this->form_validation->set_rules('clang', 'Language', 'required');
-            $this->form_validation->set_rules('tdates', 'Article Dates', 'required');
-            $this->form_validation->set_rules('tdesc', 'Article Content', 'required');
-
-            if ($this->form_validation->run($this) == TRUE)
-            {  
-                $config['upload_path']   = './images/article/';
+        if ($this->acl->otentikasi2($this->title) == TRUE){         
+ 
+                $config['upload_path']   = './uploads/';
+//                $config['upload_path']   = 'http://calculator.dswip.com/uploads/';
                 $config['file_name']     = split_space($this->input->post('ttitle'));
-                $config['allowed_types'] = 'png|jpg';
+                $config['allowed_types'] = 'png|jpg|jpeg';
                 $config['overwrite']     = TRUE;
                 $config['max_size']	 = '10000';
                 $config['max_width']     = '10000';
@@ -223,41 +273,41 @@ class Article extends MX_Controller
             if ( !$this->upload->do_upload("userfile")) // if upload failure
             {
                 $data['error'] = $this->upload->display_errors();
-                
-                $article = array(
-                'title' => $this->input->post('ttitle'), 'category_id' => $this->input->post('ccategory'),
-                'permalink' => split_space($this->input->post('ttitle')),
-                'lang' => $this->input->post('clang'), 'image' => null, 'user' => $this->session->userdata('username'),
-                'comment' => $this->cek_tick($this->input->post('ccoment')), 'front' => $this->cek_tick($this->input->post('cfront')),
-                'dates' => setnull($this->input->post('tdates')), 'time' => waktuindo(), 'text' => $this->input->post('tdesc'),
-                'created' => date('Y-m-d H:i:s'));
-                
+                print_r($data['error']);
             }
             else
             {
                 $info = $this->upload->data();
-                $article = array(
-                'title' => $this->input->post('ttitle'), 'category_id' => $this->input->post('ccategory'),
-                'permalink' => split_space($this->input->post('tpermalink')),
-                'lang' => $this->input->post('clang'), 'image' => $info['file_name'], 'user' => $this->session->userdata('username'),
-                'comment' => $this->cek_tick($this->input->post('ccoment')), 'front' => $this->cek_tick($this->input->post('cfront')),
-                'dates' => setnull($this->input->post('tdates')), 'time' => waktuindo(), 'text' => $this->input->post('tdesc'),
-                'created' => date('Y-m-d H:i:s'));
-            }
-
-                $this->Article_model->add($article);
-                $this->session->set_flashdata('message', "One $this->title data successfully saved!");
-//                if ($this->upload->display_errors()){ echo "warning|".$this->upload->display_errors(); }
-//                else { echo 'true|Data successfully saved..!|'.base_url().'images/article/'.$info['file_name']; }
                 
-                redirect($this->title.'/add/');
+                //File path at local server
+                $source = 'uploads/'.$info['file_name'];
+                
+                                //Load codeigniter FTP class
+                $this->load->library('ftp');
+                
+                //FTP configuration
+                $ftp_config['hostname'] = 'ftp.dswip.com'; 
+//                $ftp_config['hostname'] = 'ftp.103.247.11.242'; 
+                $ftp_config['username'] = 'dv200135';
+                $ftp_config['password'] = 'Jsu74gYStdh6';
+                $ftp_config['debug']    = FALSE;
+                
+                //Connect to the remote server
+                $this->ftp->connect($ftp_config);
+                
+                //File upload path of remote server
+                $destination = '/calculator.dswip.com/uploads/'.$info['file_name'];
+                
+                 //Upload file to the remote server
+                $stts = $this->ftp->upload($source, ".".$destination);
+                //Close FTP connection
+                $this->ftp->close();
+                
+                if ($stts == TRUE){ echo 'success transfer'; @unlink($source); }else{ echo 'failed transfer'; }
+                
             }
-            else
-            {
-                $this->load->view('template', $data);
-    //            echo validation_errors();
-                //echo 'error|'.validation_errors();
-            }
+            
+           
         }
         else { echo "error|Sorry, you do not have the right to edit $this->title component..!"; }
     }
@@ -273,9 +323,44 @@ class Article extends MX_Controller
     
     function remove_img($id)
     {
-        $img = $this->Article_model->get_by_id($id)->row();
+        $img = $this->Orders_model->get_by_id($id)->row();
         $img = $img->icon;
         if ($img){ $img = "./images/component/".$img; unlink("$img"); }
+    }
+    
+            // Fungsi update untuk menset texfield dengan nilai dari database
+    function invoice($param=0,$type='invoice')
+    {
+        $orders = $this->Orders_model->get_by_id($param)->row();
+        
+        $data['title'] = $this->properti['name'].' | '.ucwords($this->modul['title']);
+        
+        if ($orders){
+                
+            // property
+            $data['p_name'] = $this->properti['sitename'];
+            $data['p_address'] = $this->properti['address'];
+            $data['p_city'] = $this->properti['city'];
+            $data['p_zip']  = $this->properti['zip'];
+            $data['p_phone']  = $this->properti['phone1'];
+            $data['p_email']  = $this->properti['email'];
+            $data['p_logo']  = $this->properti['logo'];
+
+            $data['code'] = $orders->code;
+            $data['dates'] = tglin($orders->dates);
+            $data['agent'] = $this->agent->get_name($orders->agent_id);
+            $data['content'] = $orders->content;
+            $data['image'] = base_url().'images/offer/'.$orders->image;
+            
+            if ($orders->approved == 1){ $stts = 'Processed'; }else{ $stts = 'On Progress'; }
+            $data['status'] = $stts;
+            
+             if ($type == 'invoice'){ $this->load->view('orders_invoice', $data); }
+             else{
+                $html = $this->load->view('orders_invoice', $data, true); // render the view into HTML
+                return $html;
+             }
+        }
     }
 
     // Fungsi update untuk menset texfield dengan nilai dari database
@@ -291,7 +376,7 @@ class Article extends MX_Controller
         $data['category'] = $this->category->combo();
         $data['source'] = site_url($this->title.'/getdatatable');
         
-        $article = $this->Article_model->get_by_id($uid)->row();
+        $article = $this->Orders_model->get_by_id($uid)->row();
 	$this->session->set_userdata('langid', $article->id);
         
         $data['default']['category'] = $article->category_id;
@@ -313,7 +398,7 @@ class Article extends MX_Controller
  
     function valid($val)
     {
-        if ($this->Article_model->valid('title',$val) == FALSE)
+        if ($this->Orders_model->valid('title',$val) == FALSE)
         {
             $this->form_validation->set_message('valid_modul', $this->title.' registered');
             return FALSE;
@@ -324,7 +409,7 @@ class Article extends MX_Controller
     function validating($val)
     {
 	$id = $this->session->userdata('langid');
-	if ($this->Article_model->validating('title',$val,$id) == FALSE)
+	if ($this->Orders_model->validating('title',$val,$id) == FALSE)
         {
             $this->form_validation->set_message('validating_modul', "This $this->title name is already registered!");
             return FALSE;
@@ -386,7 +471,7 @@ class Article extends MX_Controller
                 'created' => date('Y-m-d H:i:s'));
             }
 
-	    $this->Article_model->update($this->session->userdata('langid'), $article);
+	    $this->Orders_model->update($this->session->userdata('langid'), $article);
             $this->session->set_flashdata('message', "One $this->title has successfully updated!");
             redirect($this->title.'/update/'.$this->session->userdata('langid'));
           //  $this->session->unset_userdata('langid');
@@ -411,7 +496,7 @@ class Article extends MX_Controller
         
         $lang = $this->input->post('clang');
         $category = $this->input->post('ccategory');
-        $result = $this->Article_model->get_list($lang,$category)->result();
+        $result = $this->Orders_model->get_list($lang,$category)->result();
         
         // library HTML table untuk membuat template table class zebra
         $tmpl = array('table_open' => '<table id="" class="table table-striped table-bordered">');
